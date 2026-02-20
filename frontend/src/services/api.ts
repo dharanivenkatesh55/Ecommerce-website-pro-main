018987693fcfe16c3d692806e993501abce57ba5
dharanivenkatesh55 <dharanivenkatesh55@gmail.com>
2026-02-20 11:38:32 +0530

    Fix API URL

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 // const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:5000/api';
-const API_URL = "https://ecommerce-website-pro-backend.onrender.com/api"
+// const API_URL = "https://ecommerce-website-pro-backend.onrender.com/api"
+const API_URL = "https://ecommerce-website-pro-backend.onrender.com/api";
 const api = axios.create({
     baseURL: API_URL,
 });
